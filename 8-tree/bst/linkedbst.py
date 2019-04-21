fa8bb7e79463fa70c2499a8796973b7c4db35991
GYT0313 <guyongtao@qq.com>
2019-04-21 22:12:11 +0800

    二叉搜索树

--- 8-tree/bst/linkedbst.py
+++ 8-tree/bst/linkedbst.py
@@ -97,7 +97,88 @@ class LinkedBST(AbstractCollection):
         self._size += 1
 
     def remove(self, item):
-        """删除item"""
+        if not item in self:
+            raise KeyError("Item not in tree.""")
+
+        # Helper function to adjust placement of an item
+        def liftMaxInLeftSubtreeToTop(top):
+            # Replace top's datum with the maximum datum in the left subtree
+            # Pre:  top has a left child
+            # Post: the maximum node in top's left subtree
+            #       has been removed
+            # Post: top.data = maximum value in top's left subtree
+            parent = top
+            currentNode = top.left
+            while not currentNode.right == None:
+                parent = currentNode
+                currentNode = currentNode.right
+            top.data = currentNode.data
+            if parent == top:
+                top.left = currentNode.left
+            else:
+                parent.right = currentNode.left
+
+        # Begin main part of the method
+        if self.isEmpty(): return None
+        
+        # Attempt to locate the node containing the item
+        itemRemoved = None
+        preRoot = BSTNode(None)
+        preRoot.left = self._root
+        parent = preRoot
+        direction = 'L'
+        currentNode = self._root
+        while not currentNode == None:
+            if currentNode.data == item:
+                itemRemoved = currentNode.data
+                break
+            parent = currentNode
+            if currentNode.data > item:
+                direction = 'L'
+                currentNode = currentNode.left
+            else:
+                direction = 'R'
+                currentNode = currentNode.right
+                
+        # Return None if the item is absent
+        if itemRemoved == None: return None
+        
+        # The item is present, so remove its node
+
+        # Case 1: The node has a left and a right child
+        #         Replace the node's value with the maximum value in the
+        #         left subtree
+        #         Delete the maximium node in the left subtree
+        if not currentNode.left == None \
+           and not currentNode.right == None:
+            liftMaxInLeftSubtreeToTop(currentNode)
+        else:
+            
+        # Case 2: The node has no left child
+            if currentNode.left == None:
+                newChild = currentNode.right
+                
+        # Case 3: The node has no right child
+            else:
+                newChild = currentNode.left
+                
+        # Case 2 & 3: Tie the parent to the new child
+            if direction == 'L':
+                parent.left = newChild
+            else:
+                parent.right = newChild
+            
+        # All cases: Reset the root (if it hasn't changed no harm done)
+        #            Decrement the collection's size counter
+        #            Return the item
+        self._size -= 1
+        if self.isEmpty():
+            self._root = None
+        else:
+            self._root = preRoot.left
+        return itemRemoved
+
+        """
         if self.find(item) == None:
             raise KeyError("Item not in tree.")
 
@@ -163,3 +244,4 @@ class LinkedBST(AbstractCollection):
         else:
             self._root = pre_root.left
         return item_removed
+        """
